bb01d81108b31e01b72047b4de82043b32aceb8c
ena-isme <dev.shinena@gmail.com>
2025-06-05 19:15:14 +0900

    add package

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -5,8 +5,15 @@ from pydantic import BaseModel
 from pathlib import Path
 import json
 import os
+import logging
+import time
+from multiprocessing import Queue
+from os import getenv
+from fastapi import Request
 
 from prometheus_fastapi_instrumentator import Instrumentator
+
+from logging_loki import LokiQueueHandler
 
 app = FastAPI()
 
